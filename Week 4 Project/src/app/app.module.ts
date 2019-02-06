de5af97833044413c233544b1a7393e78e480996
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2019-02-06 14:31:44 +0530

    commit

--- Week 4 Project/src/app/app.module.ts
+++ Week 4 Project/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +17,9 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Week 4 Project/src/app/signup/signup.component.ts
+++ Week 4 Project/src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  registerForm: FormGroup;
+    submitted = false;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            phnum: ['', [Validators.required, Validators.minLength(10)]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+        // console.log(controls.firstName.touched);
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+    onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      alert('SUCCESS!! :-)');
   }
-
 }
